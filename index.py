b324213179e5d3261c94aa046463478c0622592b
jose.ferreira <jose.edivaldo.fisica@gmail.com>
2023-08-22 13:05:36 -0300

    Adjusting index

--- index.py
+++ index.py
@@ -1,13 +1,20 @@
-from dash import html
+from dash import html, dcc
 from dash.dependencies import Input, Output
 
 # Connect to main app.py file
 from app import app
 from app import server
 # from apps import page1, page2
+import dash_custom_components.my_components as c1 
 
-app.layout = html.Div([
-])
+link_list = [("01. Main Page", "/"),
+             ("02. Data Profiling", "/profiling"),
+             ]
+
+navbar, CONTENT_STYLE = c1.navbar(link_list=link_list, color="#3957BD")
+
+content = html.Div(id='page-content', style=CONTENT_STYLE)
+app.layout = html.Div([dcc.Location(id="url"), navbar, content])
 
 @app.callback(Output('page-content', 'children'), [Input('url', 'pathname')])
 def display_page(pathname):
@@ -22,8 +29,8 @@ def display_page(pathname):
     """
     if pathname == '/':
         return # page1.layout
-    if pathname == '/page2':
-        return # page2.layout
+    if pathname == '/profiling':
+        return #c1.pandas_profiling_layout(rf'assets/profilling/{profile_file_name}.html')
     else:
         return '404 Page not found'
 
